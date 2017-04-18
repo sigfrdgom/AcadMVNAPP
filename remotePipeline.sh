558e01af64e4e0972fa5a8b89ddd9ca2438d32d4
sigfridGom <sigfridgom@gmail.com>
2017-04-18 12:49:30 -0600

    listo

--- remotePipeline.sh
+++ remotePipeline.sh
@@ -11,15 +11,15 @@ echo termino enterprise
 cd /var/lib/jenkins/workspace/sigfridPipeline/proyecto/CasosAcadAppMvn-ear/target/ && cp CasosAcadAppMvn-ear-1.0-SNAPSHOT.ear /var/lib/jenkins/workspace/sigfridPipeline/compose/glassfish/
 echo copio el ear
 cd /var/lib/jenkins/workspace/sigfridPipeline/compose/
-docker-compose stop
-echo error si no esta activo
-docker rm compose_glassfish_1
-echo eliminando viejo contenedor
-cd /var/lib/jenkins/workspace/sigfridPipeline/compose/glassfish/
-./glass.sh
-echo contruyendo la nueva imagen
-cd /var/lib/jenkins/workspace/sigfridPipeline/compose/
-docker-compose up
-echo finalizo con exito
+#docker-compose stop
+#echo error si no esta activo
+#docker rm compose_glassfish_1
+#echo eliminando viejo contenedor
+#cd /var/lib/jenkins/workspace/sigfridPipeline/compose/glassfish/
+#./glass.sh
+#echo contruyendo la nueva imagen
+#cd /var/lib/jenkins/workspace/sigfridPipeline/compose/
+#docker-compose up
+#echo finalizo con exito
 
 
