ee764774b35c6c470ea5f9f81a118be4310cf4c7
sigfridGom <sigfridgom@gmail.com>
2017-04-18 13:18:46 -0600

    prueba en production

--- localPipeline.sh
+++ localPipeline.sh
@@ -3,19 +3,19 @@
 
 echo funciona por favor....................................................................................................................................................
 
-cd /home/sigfrid/tarea2/nS && mvn clean install
+cd /home/sigfrid/acadMVNAPP/libreria && mvn clean install
 echo termino libreria
-cd /home/sigfrid/tarea2/Acad &&mvn clean install
+cd /home/sigfrid/acadMVNAPP/proyecto &&mvn clean install
 echo termino enterprise
-cd /home/sigfrid/tarea2/Acad/CasosAcadAppMvn-ear/target/ && cp CasosAcadAppMvn-ear-1.0-SNAPSHOT.ear /home/sigfrid/tarea2/compose/glassfish/
+cd /home/sigfrid/acadMVNAPP/proyecto/CasosAcadAppMvn-ear/target/ && cp CasosAcadAppMvn-ear-1.0-SNAPSHOT.ear /home/sigfrid/acadMVNAPP/compose/glassfish/
 echo copio el ear
-cd /home/sigfrid/tarea2/compose/ && docker-compose stop
+cd /home/sigfrid/acadMVNAPP/compose/ && docker-compose stop
 echo error si no esta activo
 docker rm compose_glassfish_1
 echo eliminando viejo contenedor
-cd /home/sigfrid/tarea2/compose/glassfish/ && ./glass.sh
+cd /home/sigfrid/acadMVNAPP/compose/glassfish/ && ./glass.sh
 echo contruyendo la nueva imagen
-cd /home/sigfrid/tarea2/compose/ && docker-compose up -d
+cd /home/sigfrid/acadMVNAPP/compose/ && docker-compose up -d
 echo finalizo con exito
 
 
